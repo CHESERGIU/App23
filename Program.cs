edfb26ba07b87d8b3619dfbac3701d735ed3addd
Che Sergiu <37384588+CHESERGIU@users.noreply.github.com>
2020-07-03 10:52:34 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace App23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;            
+            int sum = 0;
+            do
+            {
+                input = Console.ReadLine();
+                if (input == "x")
+                    break;
+                sum += int.Parse(input);
+            } while (!(input == "x"));
+            Console.WriteLine(sum);            
+        }
+    }
+}
